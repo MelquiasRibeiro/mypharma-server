bee31575832e0e850e1f629d0a229fc9b1833990
Melquias Ribeiro <melquias10.mr@gmail.com>
2022-03-11 12:25:41 -0300

    feat: conmect aplication to mongoDb

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config()
 
 const port = process.env.PORT||"3333"
 
+const databaseUrl = process.env.MONGO_URL || ""
 
+mongoose.connect(databaseUrl)
+.then(()=>console.log("Conected to MongoDB"))
+.catch(err=>console.log(err,"err"));
 const app = express();
 app.use(cors());
 app.use(express.json());
